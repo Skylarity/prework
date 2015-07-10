db4ca9b84d522953fc99094915b9d1cbe1b0b4fa
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-09 19:46:27 -0600

    Changed date font color.

--- index.php
+++ index.php
@@ -2,9 +2,22 @@
 
 <html>
 	<head>
+		<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="style.css">
 		<title>I completed the prework</title>
 	</head>
 	<body>
-		<p>I completed the prework.</p>
+		<?php
+
+		$statement = "<h1>I completed the prework!</h1>";
+		echo $statement;
+
+		echo "<span class='date'>Date: ", longdate(time()), "</span>";
+
+		function longdate($timestamp) {
+			return date("l F jS Y", $timestamp);
+		}
+
+		?>
 	</body>
 </html>
