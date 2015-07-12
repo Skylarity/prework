a174cbc895dbc6974e30cbbbeb06a19aaa70d63f
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-11 20:50:28 -0600

    Changed spaces to tabs.

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 
 <html>
-	<head>
-		<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" type="text/css" href="style.css">
-		<title>I completed the prework</title>
-	</head>
-	<body>
-		<?php
+   <head>
+      <link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <title>I completed the prework</title>
+   </head>
+   <body>
+      <?php
 
-		$statement = "<h1>I completed the prework!</h1>";
-		echo $statement;
+      $statement = "<h1>I completed the prework!</h1>";
+      echo $statement;
 
-		echo "<span class='date'>Date: ", longdate(time()), "</span>";
+      echo "<span class='date'>Date: ", longdate(time()), "</span>";
 
-		function longdate($timestamp) {
-			return date("l F jS Y", $timestamp);
-		}
+      function longdate($timestamp) {
+         return date("l F jS Y", $timestamp);
+      }
 
-		?>
-	</body>
+      ?>
+   </body>
 </html>
